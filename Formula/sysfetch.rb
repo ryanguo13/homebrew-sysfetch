d76ee66fa4f2a6ee8de260c73acd89353edaf277
Ryan Kwok <31926419+ryanguo13@users.noreply.github.com>
2024-09-17 15:31:41 +0800

    Update sysfetch.rb

--- Formula/sysfetch.rb
+++ Formula/sysfetch.rb
@@ -2,7 +2,7 @@ class Sysfetch < Formula
   desc "A simple system information tool written in Julia"
   homepage "https://github.com/ryanguo13/sysfetch"
   url "https://github.com/ryanguo13/homebrew-sysfetch/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "28cc226b5547cd5329188d3a0dc75fdc502d2af6b4fed85be7215810fa9f9436"
+  sha256 "18664c5d3a56784bf42b25d1d6a8c44488c4ee67dc1eb9c5268d36f2d36f0078"
   license "MIT"
 
   depends_on "julia" 
